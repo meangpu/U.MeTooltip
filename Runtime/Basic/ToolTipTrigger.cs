de9e51c8dcc849a5d691d22b4b9a39dde8427154
meangpu <51825872+meangpu@users.noreply.github.com>
2024-02-22 21:58:05 +0700

    stable

--- Runtime/Basic/ToolTipTrigger.cs
+++ Runtime/Basic/ToolTipTrigger.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 namespace Meangpu.Tooltip.Basic
 {
-    public class ToolTipTrigger : BaseTooltipTrigger
+    public class TooltipTrigger : BaseTooltipTrigger
     {
         [SerializeField] string header;
         [TextArea][SerializeField] string content;
+
         public override void ShowContent() => ActionTooltip.OnDisplayBasicText?.Invoke(content, header);
     }
 }
--- Runtime/Readme.md
+++ Runtime/Readme.md
@@ -1,1 +1,6 @@
 original learn base from this : https://www.youtube.com/watch?v=HXFoUGw7eKk (game dev guide / UI god)
+
+### my new system 22-Feb-2024
+
+this system use `TooltipTrigger` as main method to setup `Tooltip` data
+the setup process should be in `Action` event function
